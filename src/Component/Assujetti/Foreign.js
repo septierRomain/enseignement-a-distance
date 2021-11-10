28421451fee1d16d9c50ccc68f0bffa8573ca905
Gizmo675 <41105943+Gizmo675@users.noreply.github.com>
2021-11-10 10:24:23 +0100

    Wording

--- src/Component/Assujetti/Foreign.js
+++ src/Component/Assujetti/Foreign.js
@@ -24,19 +24,19 @@ const Foreign = () => {
       <div className="breadcrumbs">
         <p><img src={Check} alt="fleche grise"/>Le preneur est assujetti à la TVA</p>
         <p><img src={Check} alt="fleche grise"/>Le preneur n'est pas établi en France</p>
-        <p><img src={Check} alt="fleche grise"/>Prestations entrant dans les régles générales</p>
+        <p><img src={Check} alt="fleche grise"/>Prestations d'enseignement à distance automatisées</p>
       </div>
 
       <hr />
       <div class='square'>
-        <p>En dehors des cas spécifiques mentionnés précédemment, les prestations de services réalisées au profit d'un assujetti à la TVA ne sont pas imposables en France lorsque le preneur assujetti est établi hors de France. <br />
+        <p>Les prestations de services réalisées au profit d'un assujetti à la TVA ne sont pas imposables en France lorsque le preneur assujetti est établi hors de France. <br />
         Le lieu d'imposition est alors le pays du preneur (Autre pays d'UE ou pays tiers).</p>
       </div>
       <hr />
 
-      <h4>Régle générale d'imposition :</h4>
+      <h4>Régle d'imposition :</h4>
       <ul><img src={Arrow} alt="Fleche verte" style={{transform: 'rotate(180deg)'}} />La prestation est imposable dans le pays du preneur</ul>
-      <ul><img src={Arrow} alt="Fleche verte" style={{transform: 'rotate(180deg)'}} />Appliquer le taux de TVA du pays du preneur  (sauf existence d'un dispositif d'autoliquidation - facturation HT le cas échéant)</ul>
+      <ul><img src={Arrow} alt="Fleche verte" style={{transform: 'rotate(180deg)'}} />Appliquez le taux de TVA du pays du preneur  (sauf existence d'un dispositif d'autoliquidation - facturation HT le cas échéant)</ul>
       <ul><img src={Arrow} alt="Fleche verte" style={{transform: 'rotate(180deg)'}} />Vous devez vous enregistrer à la TVA auprès de ce pays et y effectuer votre déclaration (sauf existence d'un dispositif d'autoliquidation)</ul>
       <ul><img src={Arrow} alt="Fleche verte" style={{transform: 'rotate(180deg)'}} />Un déclaration d'échange de services est à effectuer dès le 1er euro facturé</ul>
 
